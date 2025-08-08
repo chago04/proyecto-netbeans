7c5c66a630634af0beba63ed30f1880c49fcf4a4
I-LOVE-EMMA-MYERS <villagomezoscar297@gmail.com>
2025-08-08 13:42:28 -0600

    Realize el modelo del proyecto

--- src/modelo/Alumno.java
+++ src/modelo/Alumno.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// modelo/Alumno.java
 package modelo;
 
-/**
- *
- * @author villa
- */
 public class Alumno {
-    
+    private String nombre;
+    private int edad;
+    private String carrera;
+    private int cuatrimestre;
+    private double promedio;
+
+    public Alumno(String nombre, int edad, String carrera, int cuatrimestre, double promedio) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.carrera = carrera;
+        this.cuatrimestre = cuatrimestre;
+        this.promedio = promedio;
+    }
+
+    public String getNombre() { return nombre; }
+    public int getEdad() { return edad; }
+    public String getCarrera() { return carrera; }
+    public int getCuatrimestre() { return cuatrimestre; }
+    public double getPromedio() { return promedio; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setEdad(int edad) { this.edad = edad; }
+    public void setCarrera(String carrera) { this.carrera = carrera; }
+    public void setCuatrimestre(int cuatrimestre) { this.cuatrimestre = cuatrimestre; }
+    public void setPromedio(double promedio) { this.promedio = promedio; }
 }
