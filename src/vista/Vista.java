c9785c8e9e2a8fb5546f55c1eaa728607f260c75
I-LOVE-EMMA-MYERS <villagomezoscar297@gmail.com>
2025-08-08 14:15:12 -0600

    Agregar Vista

--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -4,6 +4,8 @@
  */
 package vista;
 
+import controlador.GrupoControlador;
+
 /**
  *
  * @author villa
@@ -14,7 +16,8 @@ public class Vista {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        GrupoControlador controlador = new GrupoControlador();
+        controlador.iniciar();
     }
     
 }
